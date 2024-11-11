c2641249c8189e0258e593328d9148c180f90af0
Anna Karutina <anna.karutina@khk.ee>
2024-11-11 14:41:59 +0200

    Practice 1 - using ArrayList

--- pr1.ps1
+++ pr1.ps1
@@ -3,12 +3,12 @@ $array1 = @(1, 2, 3)
 $array2 = @(4, 5, 6)
 
 # define empty result array
-$array3 = @()
+$array3 = New-Object -TypeName System.Collections.ArrayList ## $array3 = [System.Collections.ArrayList]@()
 
 # add new values to empty array
-$array3 += $array1[0] + $array2[0]
-$array3 += $array1[1] + $array2[1]
-$array3 += $array1[2] + $array2[2]
+$array3.Add($array1[0] + $array2[0]) | Out-Null
+$array3.Add($array1[1] + $array2[1]) | Out-Null
+$array3.Add($array1[2] + $array2[2]) | Out-Null
 
 # output result array
 $array3
