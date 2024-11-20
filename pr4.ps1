2dac70fa248249361e4dab5bce9b3fc7425a81b0
Anna Karutina <anna.karutina@khk.ee>
2024-11-20 10:17:32 +0200

    Practice4 - i part with input type control

---
+++ pr4.ps1
@@ -0,0 +1,22 @@
+﻿$number1 = Read-Host "Insert first number"
+$number2 = Read-Host "Insert second number"
+
+
+if($number1 -notmatch '^\d+$' -or $number1 -notmatch '^\d+$'){
+    Write-Host "Use only numbers" -ForegroundColor Red
+} else {
+
+    $biggerNumber = 0
+
+    if($number1 -gt $number2){
+        $biggerNumber = $number1
+    } elseif ($number2 -gt $number1){
+        $biggerNumber = $number2
+    } else {
+        $biggerNumber = $number1
+        Write-Host "Numbers are equal, the biggest is: $biggerNumber"
+        Exit 0
+    }
+
+    Write-Host "The higher number is : $biggerNumber"
+}
