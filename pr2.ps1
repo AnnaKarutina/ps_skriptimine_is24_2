5e084e3242bc49ee035e6b632b6a6b4843cab1b4
Anna Karutina <anna.karutina@khk.ee>
2024-11-20 08:53:39 +0200

    Practice 2 - with for loop

---
+++ pr2.ps1
@@ -0,0 +1,17 @@
+﻿$names = @("John","Joe", "Mary")
+$dayWorked = @(12, 20, 18)
+$salaryPerDay = @(100, 120, 150)
+
+$ht1 = @{"Name"=$names; "DayWorked"=$dayWorked}
+$ht2 = @{"Name"=$names; "SalaryPerDay"=$salaryPerDay}
+
+$salary = @()
+
+for($i = 0; $i -lt $names.Count; $i++){
+    $salary += $ht1.DayWorked[$i] * $ht2.SalaryPerDay[$i]
+}
+
+
+$ht3 = @{"Name"=$ht1.Name; "Salary"=$salary}
+
+$ht3
