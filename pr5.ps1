3e357ede071da14908b15feffa6de78e9d644f72
Anna Karutina <anna.karutina@khk.ee>
2024-11-25 14:01:51 +0200

    Practice 5 - ii part

--- pr5.ps1
+++ pr5.ps1
@@ -1,1 +1,9 @@
-﻿Get-Process | Where{$_.ProcessName -eq 'notepad'} | Select ProcessName, Id
+﻿Get-ChildItem -Path C:\Temp\Test
+
+Write-Host
+
+$result = Get-ChildItem -Path C:\Temp\Test | Where{$_.Name -like '*.csv'} | Select Name, Length
+
+Write-Host "Filename: $($result.Name)"
+Write-Host "Size in KB: $($result.Length/1024)"
+Write-Host "Size in MB: $($result.Length/1MB)"
